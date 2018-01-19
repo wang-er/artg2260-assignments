e4c3b93c421816f4fdee2fecbcd84c96dd962c5e
wang-er <33679181+wang-er@users.noreply.github.com>
2018-01-18 21:21:35 -0500

    added exercise 1

---
+++ exercises/exercise_1/sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+
+	//just inserting random values for all these shapes and colors
+	createCanvas(600, 600);
+	background(20);
+
+	stroke(10,255,69);
+	strokeWeight(10);
+	fill(100, 100, 255);
+	rect(300, 300, 200, 300);
+
+
+	stroke(255,108,66);
+	strokeWeight(20);
+	fill(100, 255, 40);
+	triangle(500, 500,  100, 500, 250, 100);
+
+	stroke(40,100,255);
+	strokeWeight(30);
+	fill(255, 255, 255);
+	ellipse(500, 530, 50, 50);
+
+	stroke(255,255,255);
+	strokeWeight(40);
+	fill(0, 0, 0, 60);
+	ellipse(200, 375, 220, 100);
+
+}
+		
